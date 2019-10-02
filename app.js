d0c263266ddf4ec128d01a95ce295089a464771b
rlisiecki1103 <56075977+rlisiecki1103@users.noreply.github.com>
2019-10-02 10:46:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
